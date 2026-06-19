8068ce8c7430d51bb791ba7709dbea614367c0b0
ogrickshawwala <manas@rickshawgamestudios.com>
2026-06-19 11:04:17 +0530

    deleting unwanted files

--- _preview_launch.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM Wrapper so the preview tool can launch the dashboard with Node on PATH.
-set "PATH=%PATH%;C:\Program Files\nodejs"
-cd /d "G:\Automation\PACTS\PACTS_Dashboard"
-npm run preview -- --port 5173 --host
